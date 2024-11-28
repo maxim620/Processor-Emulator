5f8e2613e5ed1de69ef121e8d6cd37bd5b831ced
maxim620 <74681933+maxim620@users.noreply.github.com>
2024-11-28 14:04:03 +0500

    Update processor.h

--- processor.h
+++ processor.h
@@ -3,21 +3,9 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include "instructions.h"
 
 #define RAM_SIZE 65536
-#define HALT 0x0000
-#define MOV 0x0001  // move
-#define MOVI 0x0002 // move immediate
-#define INC 0x0003  // increment
-#define ADD 0x0004  // add
-#define ADDI 0x0005 // add immediate
-#define SUB 0x0006  // subtract
-#define SUBI 0x0007 // subtract immediate
-#define MUL 0x0008  // multiply
-#define MULI 0x0009 // multiply immediate
-#define DIV 0x000A  // divide
-#define DIVI 0x000B // divide immediate
-#define XCHG 0x000C // exchange
 
 typedef struct
 {
